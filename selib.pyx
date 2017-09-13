8b7fc3319e545270892e52ad2753e1ff71dc8893
Noud Aldenhoven <noud.aldenhoven@screenpointmed.com>
2017-09-13 15:00:11 +0200

    Initial Simple Encryption Library for cython.

---
+++ selib.pyx
@@ -0,0 +1,19 @@
+from libcpp.string cimport string
+
+cdef extern from 'rot13.cpp':
+    cpdef string cpp_rot13(string);
+
+def rot13(i):
+    '''Rotate by 13 places (ROC) encryption.
+
+    Each letter is replaced with the letter 13 letters after it in the alphabet.
+    ROT13 is a special case of the Ceasar cipher. ROT13 is it's own inverse,
+    which means if you apply ROT13 twice you get your original message back.
+
+    Args:
+        i (string): input string to convert.
+
+    Returns:
+        Encrypted/decrypted string.
+    '''
+    return cpp_rot13(i)
